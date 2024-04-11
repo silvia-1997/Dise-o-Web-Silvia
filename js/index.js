8b034d6fc333a20128f7a59e3a0953589039f0dd
Silvia Juanita Godinez Sanchez <148423198+silvia-1997@users.noreply.github.com>
2024-04-11 03:17:45 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,25 +1,13 @@
-/**
- * Definiciones de constantes para poder ligarlas con los archivos independientes de js y poder hacer las cartas
- */
-
+/** * Definiciones de constantes para poder ligarlas con los archivos independientes de js y poder hacer las cartas */
 import {Ruta} from "./entities/Rutas.js";
 
-/**
- * Establece el monto en pesos 
- */
-
+/**Establece el monto en pesos */
 const formatoPesosMexicanos = new Intl.NumberFormat('es-MX');
 
-/**
- * Crear el arreglo para ruta, caminatas y bicis
- */
-
+/*** Crear el arreglo para ruta, caminatas y bicis */
 const rutas =[];
 
-/**
- * Variables que hacen referencia a contenedores  
- */
-
+/** * Variables que hacen referencia a contenedores*/
 const contenedorRutas = document.getElementById("contenedorRutas");
 
 /** no cargar rutas, sin declararlas */
@@ -46,7 +34,6 @@ const crearCardRuta =(ruta) => {
     botonReserva.classList.add("btn-success");
     botonReserva.innerText="Apartar lugar";
     botonReserva.onclick = creatEventoReservaRuta(ruta);
-
 
     //Footer 
     let pieCard =document.createElement("div");
@@ -89,10 +76,7 @@ const crearCardRuta =(ruta) => {
 
 }
 
-/**
- * Se encargara de recorrer la lista de productos y agregar un componente card con la info del producto al contenedor de elementos
- */
-
+/** * Se encargara de recorrer la lista de productos y agregar un componente card con la info del producto al contenedor de elementos */
 const dibujarCatalogoRutas = () => {
     contenedorRutas.innerHTML=""; 
 
@@ -102,38 +86,159 @@ const dibujarCatalogoRutas = () => {
     });
 };
 
-/**
- * Ejecucion de funciones
- */
+/** * Ejecucion de funciones */
 cargarRutas ();
 dibujarCatalogoRutas();
 
 
+/** *------------------------------DECLARACION DEL SEGUNDO TIPO DE PRODUCTO CAMINATAS--------------------------------------------------------------------------------------- */
 
-/**
- *----------------------------------------------------------------------------------------------------------------------------
- */
+import { Caminatas } from "./entities/Caminata.js";
+const caminatas = [];
+const contenedorCaminatas = document.getElementById("contenedorCaminatas");
+const cargarCaminatas = () => {
+    caminatas.push(new Caminatas('Tranqui Tequexquinahuac', 'EDO. MEX', 800));
+    caminatas.push(new Caminatas('Entrenamiento', 'EDO. MEX', 800));
+    caminatas.push(new Caminatas('Prevención de incendios', 'EDO. MEX', 800));
+    caminatas.push(new Caminatas('Punto trino', 'EDO. MEX', 800));
+};
 
- const cargarCaminatas =() => {
-    caminatas.push(new Caminata('Tranqui Tequexquinahuac', 'EDO. MEX', 800, 'Caminata matutina' ));
-    caminatas.push(new Caminata('Entrenamiento', 'EDO. MEX', 800, 'Mejora de técnica' ));
-    caminatas.push(new Caminata('Prevención de incendios', 'EDO. MEX', 800, 'Grupo scout de San Luis Huexotla' ));
-    caminatas.push(new Caminata('Punto trino', 'EDO. MEX', 800, 'Caminata matutina' ));
-
-}
-
-/**
- *----------------------------------------------------------------------------------------------------------------------------
- */
-
-const cargaBicis =() => {
-    bicis.push(new Bicis('Talon', 20000, './img/TALON.jpg', 'Rigida de aluminio con suspension ofrece un manejo equilibrado y seguro en el camino' ));
-    bicis.push(new Bici('MTB (MOUNTAIN BIKE)', 20000, './img/MTBMUJER.jpg', 'Con cuadro de aluminio calidad ALUXX y una tijera de suspensión de 100 mm de recorrido.' ));
-    bicis.push(new Bici('BENOTTO', 20000, './img/BENOTTO.jpg', 'Unisex, Marca benotto, de aluminio, 14 velocidades' ));
-    bicis.push(new Bici('LUXTRAIL', 20000, './img/LUXTRAIL.jpg', 'Pedalea a toda velocidad y con total confianza sobre esta bicicleta down country. Con espacio de almacenamiento interno en el cuadro,suspensión FOX y tija telescópica,sube el nivel de tus rutas mtb con la Lux Trail CF 6.' ));
-    bicis.push(new Bici('PIQUE29', 20000, './img/PIQUE29.jpg', 'Rines de 29 pulgadas, recorrido de 100 mm, dropper y cuadro de aluminio' ));
-    bicis.push(new Bici('PINARELLOF7', 20000, './img/PINARELLOF7.jpg', 'Cuadro de carbon, transmisión Shimano Ultegra Di2' ));
-}
+const creatEventoReservaCaminata = (caminata) => {
+    const evento = (ev) => {
+        Swal.fire("Suerte en la ruta: " + caminata.nombre);
+    };
+    return evento;
+};
 
 
+const crearCardCaminata = (caminata) => {
+    // Botón de Reserva
+    let botonReserva = document.createElement("button");
+    botonReserva.classList.add("btn");
+    botonReserva.classList.add("btn-success");
+    botonReserva.innerText = "Apartar lugar";
+    botonReserva.onclick = creatEventoReservaCaminata(caminata);
+
+    // Footer de la Card
+    let pieCardCaminata = document.createElement("div");
+    pieCardCaminata.className = "card-footer text-end";
+    pieCardCaminata.appendChild(botonReserva);
+
+   
+
+    // Cuerpo de la Card
+    let cuerpoCardCaminata = document.createElement("div");
+    cuerpoCardCaminata.className = "card-body";
+    cuerpoCardCaminata.innerHTML = `
+        <h5 class="card-title">${caminata.nombre}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">${caminata.lugar}</h6>
+        <p class="card-text">Costo: $${caminata.precio}</p>
+    `;
+
+    // Card completa
+    let card = document.createElement("div");
+    card.className = 'card';
+    card.append(cuerpoCardCaminata);
+    card.append(pieCardCaminata);
+
+    // Crear una columna (col) para la tarjeta
+    let columna = document.createElement("div");
+    columna.className = "col";
+    columna.appendChild(card);
+
+    return columna;
+};
+
+// Función para dibujar todas las caminatas en el contenedor
+const dibujarCatalogoCaminatas = () => {
+    contenedorCaminatas.innerHTML = "";
+
+    caminatas.forEach(caminata => {
+        let contenedorCarta = crearCardCaminata(caminata);
+        contenedorCaminatas.appendChild(contenedorCarta);
+    });
+};
+
+// Cargar las caminatas y dibujarlas en el contenedor
+cargarCaminatas();
+dibujarCatalogoCaminatas();
+
+
+/** *-----------------------------DECLARACION DEL TERCER TIPO DE PRODUCTO BICIS-------------------------------------------------------------------------- */
+import { Bicis } from "./entities/Bicicletas.js";
+
+const bicicletas = [];
+const contenedorBicis = document.getElementById("contenedorBicis");
+
+const cargaBicis = () => {
+    bicicletas.push(new Bicis('Talon', 20000, './img/TALON.jpg'));
+    bicicletas.push(new Bicis('MTB (MOUNTAIN BIKE)', 20000, './img/MTBMUJER.jpg'));
+    bicicletas.push(new Bicis('BENOTTO', 20000, './img/BENOTTO.jpg'));
+    bicicletas.push(new Bicis('LUXTRAIL', 20000, './img/LUXTRAIL.jpg'));
+    bicicletas.push(new Bicis('PIQUE29', 20000, './img/PIQUE29.jpg'));
+    bicicletas.push(new Bicis('PINARELLOF7', 20000, './img/PINARELLOF7.jpg'));
+};
+
+const creatEventoReservaBicis = (bicis) => {
+    const evento = (ev) => {
+        Swal.fire("Suerte en la ruta: " + bicis.nombre);
+    };
+    return evento;
+};
+
+const crearCardBicis = (bicis) => {
+    // Botón de Reserva
+    let botonReserva = document.createElement("button");
+    botonReserva.classList.add("btn", "btn-success");
+    botonReserva.innerText = "Comprar";
+    botonReserva.onclick = creatEventoReservaBicis(bicis);
+
+
+    // Cuerpo de la Card
+    let cuerpoCardBicis = document.createElement("div");
+    cuerpoCardBicis.className = "card-body";
+    cuerpoCardBicis.innerHTML = `
+        <h5 class="card-title">${bicis.nombre}</h5>
+        <h6 class="card-text">Costo: $${bicis.precio}</h6>
+    `;
+
+    // Imagen de la Bicicleta
+    let imagenBici = document.createElement("img");
+    imagenBici.src = bicis.urlfoto;
+    imagenBici.className = "card-img-top";
+    imagenBici.alt = bicis.nombre;
+
+    // Footer de la Card
+    let pieCardBicis = document.createElement("div");
+    pieCardBicis.className = "card-footer text-end";
+    pieCardBicis.appendChild(botonReserva);
+
+    // Card completa
+    let card = document.createElement("div");
+    card.className = 'card';
+    card.appendChild(imagenBici); // Agregar la imagen al card-body
+    card.appendChild(cuerpoCardBicis);
+    card.appendChild(pieCardBicis);
+
+    // Crear una columna (col) para la tarjeta
+    let columna = document.createElement("div");
+    columna.className = "col";
+    columna.appendChild(card);
+
+    return columna;
+};
+
+// Función para dibujar todas las bicicletas en el contenedor
+const dibujarCatalogoBicis = () => {
+    contenedorBicis.innerHTML = "";
+
+    bicicletas.forEach(bicis => {
+        let contenedorCarta = crearCardBicis(bicis);
+        contenedorBicis.appendChild(contenedorCarta);
+    });
+};
+
+// Cargar las bicicletas y dibujarlas en el contenedor
+cargaBicis();
+dibujarCatalogoBicis();
 
